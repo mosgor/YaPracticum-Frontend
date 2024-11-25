db406e42ea31440158606c4ebc384dd43db19c80
Maxim Mikhailov <mos_gor@list.ru>
2024-11-25 21:04:53 +0300

    Finished working on Fourth Sprint

--- FourthSprint/scripts/index.js
+++ FourthSprint/scripts/index.js
@@ -17,63 +17,82 @@ const cardNameInput = cardFormElement.querySelector('.popup__input_type_card-nam
 const cardLinkInput = cardFormElement.querySelector('.popup__input_type_url');
 
 const imagePopup = document.querySelector('.popup_type_image');
+const popupImageElement = imagePopup.querySelector('.popup__image');
+const popupCaptionElement = imagePopup.querySelector('.popup__caption');
 
 // Функция создания карточки
 function createCard(name, link) {
-    const card = cardTemplate.querySelector('.card').cloneNode(true);
-    card.querySelector('.card__title').textContent = name;
-    const cardImage = card.querySelector('.card__image');
-    cardImage.setAttribute("src", link);
-    cardImage.setAttribute("alt", name);
-    return card;
+	const card = cardTemplate.querySelector('.card').cloneNode(true);
+	card.querySelector('.card__title').textContent = name;
+	const cardImage = card.querySelector('.card__image');
+	cardImage.setAttribute("src", link);
+	cardImage.setAttribute("alt", name);
+	card.querySelector('.card__like-button').addEventListener('click', (event) => {
+		event.target.classList.toggle('card__like-button_is-active');
+	})
+	card.querySelector('.card__delete-button').addEventListener('click', handlerRemoveCard);
+    cardImage.addEventListener('click', () => {
+        popupImageElement.setAttribute('src', cardImage.getAttribute('src'));
+        popupImageElement.setAttribute('alt', cardImage.getAttribute('alt'));
+        popupCaptionElement.textContent = cardImage.getAttribute('alt');
+        openModal(imagePopup);
+    })
+	return card;
 }
 
-function handleProfileFormSubmit(evt) {
-    evt.preventDefault();
-    profileTitle.textContent = nameInput.value;
-    profileDescription.textContent = jobInput.value;
-    closeModal(profilePopup);
+function handleProfileFormSubmit(event) {
+	event.preventDefault();
+	profileTitle.textContent = nameInput.value;
+	profileDescription.textContent = jobInput.value;
+	closeModal(profilePopup);
 }
 
-function handleCardFormSubmit(evt) {
-    evt.preventDefault();
-    placesList.prepend(createCard(cardNameInput.value, cardLinkInput.value));
-    closeModal(cardPopup);
+function handleCardFormSubmit(event) {
+	event.preventDefault();
+	placesList.prepend(createCard(cardNameInput.value, cardLinkInput.value));
+	closeModal(cardPopup);
 }
 
-// @todo: Функция удаления карточки
+// Функция удаления карточки
+function handlerRemoveCard(event) {
+    event.target.closest('.card').remove();
+}
 
 function openModal(popup) {
-    popup.classList.add('popup_is-opened');
+	popup.classList.add('popup_is-opened');
 }
 
 function closeModal(popup) {
-    popup.classList.remove('popup_is-opened');
+	popup.classList.remove('popup_is-opened');
 }
 
 // Вывести карточки на страницу
 initialCards.forEach((card) => {
-    placesList.append(createCard(card.name, card.link));
+	placesList.append(createCard(card.name, card.link));
 })
 
+profilePopup.classList.add('popup_is-animated');
+cardPopup.classList.add('popup_is-animated');
+imagePopup.classList.add('popup_is-animated');
+
 document.querySelector('.profile__edit-button').addEventListener('click', () => {
-    nameInput.setAttribute('value', profileTitle.textContent);
-    jobInput.setAttribute('value', profileDescription.textContent);
-    openModal(profilePopup);
+	nameInput.value = profileTitle.textContent;
+	jobInput.value = profileDescription.textContent;
+	openModal(profilePopup);
 })
 
 profileFormElement.addEventListener('submit', handleProfileFormSubmit);
 
 document.querySelector('.profile__add-button').addEventListener('click', () => {
-    cardNameInput.value = '';
-    cardLinkInput.value = '';
-    openModal(cardPopup);
+	cardNameInput.value = '';
+	cardLinkInput.value = '';
+	openModal(cardPopup);
 })
 
 cardFormElement.addEventListener('submit', handleCardFormSubmit);
 
 document.querySelectorAll('.popup__close').forEach((popupClose) => {
-    popupClose.addEventListener('click', (event) => {
-        closeModal(event.target.closest('.popup'));
-    })
+	popupClose.addEventListener('click', (event) => {
+		closeModal(event.target.closest('.popup'));
+	})
 })
